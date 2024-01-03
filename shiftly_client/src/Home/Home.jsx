e92ac8f39443dd6d2d5a3d62fcecad8292976a7b
arielbarmats <ariel.barmats@gmail.com>
2024-01-03 23:09:41 +0200

    welcome box and input

--- shiftly_client/src/Home/Home.jsx
+++ shiftly_client/src/Home/Home.jsx
@@ -2,17 +2,33 @@ import "./Home.css";
 import HomeTopBar from "./HomeTopBar/HomeTopBar";
 
 function Home() {
-    return ( 
-        <>
-        <HomeTopBar/>
+  return (
+    <>
+      <HomeTopBar />
 
-        
+      <div id="welcomeBox" className="HomeBoxes">
+        <p id="welcomeMessage">
+          Hi, User! Welcome back to <span>Shiftly</span>.
+        </p>
+        <p>
+          Tip: In the Settings you can change your preferences which can lead to
+          different results on your tables
+        </p>
+      </div>
 
-
-        
-        
-        </>
-     );
+      <div id="HistoryContainer" className="HomeBoxes">
+        <div class="input-icons">
+          <i class="bi bi-search icon"></i>
+          <input
+            id="History_input"
+            class="input-field"
+            type="password"
+            placeholder="Search Table"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
--- shiftly_client/src/Home/HomeTopBar/HomeTopBar.js
+++ shiftly_client/src/Home/HomeTopBar/HomeTopBar.js
@@ -18,3 +18,4 @@ function HomeTopBar () {
 }
 
 export default HomeTopBar ;
+

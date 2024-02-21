e18c00cf2b1ed27401efb8caf2c1081943c72ef9
arielbarmats <ariel.barmats@gmail.com>
2024-02-21 02:16:45 +0200

    search in home, styling, remove and stared tiles

--- shiftly_client/src/Home/Home.js
+++ shiftly_client/src/Home/Home.js
@@ -1,10 +1,40 @@
 import "./Home.css";
 import HomeTopBar from "./HomeTopBar/HomeTopBar";
 import SchedulingTile from "./SchedulingTile/SchedulingTile";
-import React from 'react';
-
+import React, { useState } from "react";
 
 function Home() {
+  // State variable to hold the input value
+  const [filterValue, setFilterValue] = useState("");
+
+  const [SchedulingTiles, setSchedulingTiles] = useState([
+    { name: "week 13", date: "01/01/2024" },
+    { name: "week 14", date: "08/01/2024" },
+    { name: "week 15", date: "15/01/2024" },
+    { name: "week 16", date: "21/01/2024" },
+    { name: "week 19", date: "28/01/2024" },
+    { name: "week 19b", date: "28/01/2024" },
+    { name: "week 19c", date: "29/01/2024" },
+    { name: "week 30", date: "20/03/2024" },
+    { name: "a", date: "27/03/2024" },
+    { name: "aaaaaaabb", date: "29/03/2024" },
+  ]);
+
+  // Event handler to update the filter value
+  const handleInputChange = (event) => {
+    setFilterValue(event.target.value);
+  };
+
+  // Function to remove a tile by name
+  const removeTile = (nameToRemove) => {
+    setSchedulingTiles(SchedulingTiles.filter(tile => tile.name !== nameToRemove));
+  };
+
+  // Filtered scheduling tiles based on input value
+  const filteredTiles = SchedulingTiles.filter((tile) =>
+    tile.name.toLowerCase().includes(filterValue.toLowerCase())
+  );
+
   return (
     <>
       <HomeTopBar page="home" />
@@ -21,26 +51,28 @@ function Home() {
         </div>
 
         <div id="HomeMainBox" className="HomeBoxes">
-          <div class="input-icons">
-            <i class="bi bi-search icon"></i>
+          <div className="input-icons">
+            <i className="bi bi-search icon"></i>
             <input
               id="History_input"
-              class="input-field"
+              className="input-field"
               placeholder="Search Table"
+              value={filterValue}
+              onChange={handleInputChange}
             />
           </div>
 
           <div id="HistoryListContainer"></div>
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
-          <SchedulingTile />
+
+          {/* Render filtered tiles */}
+          {filteredTiles.map((tile, index) => (
+            <SchedulingTile
+              key={index}
+              name={tile.name}
+              date={tile.date}
+              onRemove={removeTile}
+            />
+          ))}
         </div>
       </div>
       <div id="easterEgg">easter egg</div>
--- shiftly_client/src/Home/SchedulingTile/SchedulingTile.jsx
+++ shiftly_client/src/Home/SchedulingTile/SchedulingTile.jsx
@@ -1,18 +1,35 @@
 import "./SchedulingTile.css";
-import React from 'react';
+import React, { useState } from "react";
 
+function SchedulingTile(props) {
+  // State variable to manage the filled/empty state of the star icon
+  const [isStarFilled, setIsStarFilled] = useState(false);
 
-function SchedulingTile() {
+  // Function to toggle the state of the star icon
+  const toggleStar = () => {
+    setIsStarFilled(!isStarFilled);
+  };
+
+  const handleRemoveTile = () => {
+    props.onRemove(props.name);
+  };
+
   return (
     <>
       <div id="TileContainer">
         <img src="/SchedualPic.jpg" alt="Logo" />
-        <span id="NameOfTable">Name Of Table</span>
-        <span id="dateOfTable">dd/mm/yyyy</span>
+        <span id="NameOfTable">{props.name}</span>
+        <span id="dateOfTable">{props.date}</span>
         <div className="icons">
-          <i class="bi bi-star"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-dash-circle"></i>
+          {/* Conditionally render star icon based on state */}
+          {isStarFilled ? (
+            <i className="bi bi-star-fill" onClick={toggleStar}></i>
+          ) : (
+            <i className="bi bi-star" onClick={toggleStar}></i>
+          )}
+          <button onClick={handleRemoveTile}>
+            <i className="bi bi-dash-circle"></i>
+          </button>
         </div>
       </div>
     </>
